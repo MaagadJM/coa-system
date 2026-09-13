db666a0fc7c649f622006683155551ec0b76be7b
Jude Michael Maagad <judemichael.maagad@hmr.ph>
2026-09-13 23:49:21 +0800

    fix: add BrowserRouter basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { AppRouter } from './routes/AppRouter'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/coa-system">
       <AuthProvider>
         <ProjectProvider>
           <AppRouter />
